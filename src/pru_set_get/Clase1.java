7a25ff33223831f602e6fb36f3ef9138da142861
migue <ce23002@ues.edu.sv>
2024-03-05 17:54:42 -0600

    prueba0.1

---
+++ src/pru_set_get/Clase1.java
@@ -0,0 +1,32 @@
+package pru_set_get;
+
+public class Clase1 {
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+     private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String apellido;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
